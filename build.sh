e7aa52e4a421672cceba633499c5fc0a68f25db6
Piotrek Stapp <piotr.stapp@allegro.pl>
2021-03-05 13:11:04 +0100

    Fix build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ VERSION=v_$(git rev-parse --short=5 HEAD)
 echo "build version $VERSION"
 cd HttpClientTests
 docker build -t httptest.azurecr.io/httpclienttests:$VERSION .
-az acr login
-docker push httptest.azurecr.io/httpclienttests:$VERSION
 cd ..
+az acr login --name httptest.azurecr.io
+docker push httptest.azurecr.io/httpclienttests:$VERSION
 cat deploy-disabled.yaml| envsubst | kubectl apply -f -
 cat deploy-enabled.yaml| envsubst | kubectl apply -f -
 cat deploy-test.yaml| envsubst | kubectl apply -f -
